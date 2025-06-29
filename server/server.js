2ce58379502146ba7bb240f640b89cd91475eec8
Fabricio Guacuto <guacutofabricio@gmail.com>
2025-06-29 05:21:12 -0700

    Change start script to use && instead of concurrently to ensure client is built before server starts. Fixes cold start issue where server tries to serve non-existent dist folder.

--- server/server.js
+++ server/server.js
@@ -18,16 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use('/api', routes);
-
-// Serve up static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    const clientPath = path.join(__dirname, '../client/dist');
-    app.use(express.static(clientPath));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(clientPath, 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
 
