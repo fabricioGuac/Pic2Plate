eb97af3f450ca85d766b2940bc96d81f1ead4dfc
Fabricio Guacuto <guacutofabricio@gmail.com>
2025-06-28 22:48:48 -0700

    changes the start to build the client and updates the path in server.js

--- server/server.js
+++ server/server.js
@@ -14,13 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware to parse the url encoded or json  requests
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 }
+
 
 //Set up the routes
 app.use(routes);
