dad706151b8a2c92223a991bc9e7e52f61a1db83
Fabricio Guacuto <guacutofabricio@gmail.com>
2025-06-29 05:33:24 -0700

    removes the catch all route

--- server/server.js
+++ server/server.js
@@ -23,9 +23,6 @@ app.use(routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
 }
 
 
